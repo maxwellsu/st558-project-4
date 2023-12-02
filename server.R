4abc3b8bb44657425f7781e62713fb1297ab6e95
Maxwell Su <mysu@ncsu.edu>
2023-12-02 02:33:50 -0500

    implemented model fitting

--- server.R
+++ server.R
@@ -2,6 +2,8 @@ library(shiny)
 library(tidyverse)
 library(httr)
 library(jsonlite)
+library(randomForest)
+library(caret)
 
 function(input, output, session) {
   
@@ -16,33 +18,77 @@ function(input, output, session) {
     mutate(thirdDownConversionRate = thirdDownConversions / thirdDowns) %>% 
     mutate(fourthDownConversionRate = fourthDownConversions / fourthDowns) %>%
     mutate(turnoverDiff = (passesIntercepted + fumblesRecovered) - turnovers) %>%
-    mutate(bowl.eligible = total.wins >= 6) %>% 
+    mutate(bowlEligible = total.wins >= 6) %>% 
     select(!c(year, teamId, season, conference.y, games, 
               thirdDownConversions, thirdDowns, fourthDownConversions, 
               fourthDowns, passesIntercepted, fumblesRecovered, turnovers, fumblesLost, interceptions))
   
   fbs2022[,21:42] <- fbs2022[,21:42] / fbs2022$total.games
-
+  fbs2022$conference <- as.factor(fbs2022$conference)
+  levels(fbs2022$conference) <- make.names(levels(fbs2022$conference))
+  fbs2022$bowlEligible <- as.factor(fbs2022$bowlEligible)
+  levels(fbs2022$bowlEligible) <- c("No", "Yes")
+  
   getPlotType <- reactive({
     plotType <- input$plotType
   })
-  getTableType <- reactive({
-    tableType <- input$tableType
-  })
-  getX <- reactive({
-    x <- input$x
-  })
-  getY <- reactive({
-    y <- input$y
-  })
-  getZ <- reactive({
-    z <- input$z
-  })
-  getW <- reactive({
-    w <- input$w
-  })
+  # getX <- reactive({
+  #   x <- input$x
+  # })
+  # getY <- reactive({
+  #   y <- input$y
+  # })
+  # getZ <- reactive({
+  #   z <- input$z
+  # })
+  # getW <- reactive({
+  #   w <- input$w
+  # })
+  
+  
 
-  output$Plot <- renderPlot({
+  output$glmSummary <- renderPrint({
+    set.seed(1647)
+    
+    train <- createDataPartition(1:nrow(fbs2022), p = input$trainProp, list = FALSE)
+    train_data <- fbs2022[train, c(2, 22:46)]
+    test_data <- fbs2022[-train, c(2, 22:46)]
+    
+    glmfit <- train(bowlEligible ~ conference + rushingYards + 
+                      netPassingYards + sacks + penaltyYards + puntReturnYards +
+                      thirdDownConversionRate + fourthDownConversionRate + 
+                      turnoverDiff + possessionTime,
+                    data = train_data,
+                    method = "glm",
+                    family = "binomial",
+                    trControl = trainControl(method = "cv",
+                                             number = 5,
+                                             classProbs = TRUE,
+                                             summaryFunction=mnLogLoss),
+                    metric = "logLoss")
+    summary(glmfit)
+  })
+  
+  output$rfPlot <- renderPlot({
+    set.seed(1647)
+    
+    train <- createDataPartition(1:nrow(fbs2022), p = input$trainProp, list = FALSE)
+    train_data <- fbs2022[train, c(2, 22:46)]
+    test_data <- fbs2022[-train, c(2, 22:46)]
+    
+    rffit <- train(bowlEligible ~ .,
+                   data = train_data,
+                   method = "rf",
+                   trControl = trainControl(method = "cv",
+                                            number = 5,
+                                            classProbs = TRUE,
+                                            summaryFunction=mnLogLoss),
+                   tuneGrid = data.frame(mtry = 1:5),
+                   metric = "logLoss")
+    varImpPlot(rffit$finalModel)
+  })
+  
+  output$EDAPlot <- renderPlot({
     plotType <- getPlotType()
     g <- ggplot(data = fbs2022)
     if(plotType == 2) {
@@ -57,7 +103,5 @@ function(input, output, session) {
     } else {
       g + geom_bar(aes(x = total.wins, fill = conference))
     }
-
   })
-
 }
